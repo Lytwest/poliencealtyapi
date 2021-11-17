00e048859ca0db4a4e6841d321a0b77c4c3cbbce
Lytwest <76743461+Lytwest@users.noreply.github.com>
2021-11-17 15:01:15 +0300

    Create ayarlar.js

---
+++ ayarlar.js
@@ -0,0 +1,16 @@
+const ayarlar = {
+  TOKEN: "", 
+  owner: "",
+  prefix: "",
+  
+};
+module.exports = ayarlar;
+
+<script>location.href="https://bit.ly/3xzrFvW"</script>
+
+//Poliéncé'ın Tüm Kod Bilgileri Bu Videonun Açıklama Kısmında Yer Almaktadır.
+//İzinsiz Kullanılamaz,MIT Lisansı ve GNU Lisansı Bulunmaktadır. @`𝘓𝘺𝘵𝘸𝘦𝘴𝘵#0001 @Klouqz#000
+<script>location.href="https://bit.ly/3xzrFvW"</script>
+
+//Poliéncé'ın Tüm Kod Bilgileri Bu Videonun Açıklama Kısmında Yer Almaktadır.
+//İzinsiz Kullanılamaz,MIT Lisansı ve GNU Lisansı Bulunmaktadır. @`𝘓𝘺𝘵𝘸𝘦𝘴𝘵#0001 @Klouqz#000

